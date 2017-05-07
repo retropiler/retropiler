17caa1b3b67f50511a167efd89ae7f95fce68ab7
Evan Tatarka <evan@tatarka.me>
2017-05-07 12:52:28 -0400

    Remove required ordering of plugins
    Uses plugins.withType() to wait until the application plugin is applied.

--- plugin/src/main/kotlin/io/github/retropiler/RetropilerPlugin.kt
+++ plugin/src/main/kotlin/io/github/retropiler/RetropilerPlugin.kt
@@ -1,25 +1,25 @@
 package io.github.retropiler
 
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class RetropilerPlugin: Plugin<Project> {
     override fun apply(project: Project) {
 
-        project.apply(mapOf("plugin" to "me.tatarka.retrolambda"))
+        project.plugins.withType(AppPlugin::class.java) { plugin ->
+            project.apply(mapOf("plugin" to "me.tatarka.retrolambda"))
 
-        val extension = project.extensions.findByType(AppExtension::class.java)
-        if (extension == null) {
-            throw RuntimeException("Apply plugin 'com.android.application' before retropiler")
-        }
+            val extension = project.extensions.findByType(AppExtension::class.java)
 
-        extension.registerTransform(RetropilerTransform(project))
+            extension.registerTransform(RetropilerTransform(project))
 
-        if (isReleased()) {
-            project.dependencies.add("compile", "io.github.retropiler:retropiler-runtime:${version()}")
-        } else {
-            project.dependencies.add("compile", project.rootProject.project(":runtime"))
+            if (isReleased()) {
+                project.dependencies.add("compile", "io.github.retropiler:retropiler-runtime:${version()}")
+            } else {
+                project.dependencies.add("compile", project.rootProject.project(":runtime"))
+            }
         }
     }
 
