686603537930c17e1953439ec7cf8074339d551a
FUJI Goro (gfx) <gfuji@cpan.org>
2017-05-07 18:41:07 +0900

    move byteweaving to Runtime class

--- plugin/src/main/kotlin/io/github/retropiler/RetropilerTransform.kt
+++ plugin/src/main/kotlin/io/github/retropiler/RetropilerTransform.kt
@@ -4,11 +4,6 @@ import com.android.SdkConstants
 import com.android.build.api.transform.*
 import com.android.build.gradle.AppExtension
 import javassist.ClassPool
-import javassist.CtClass
-import javassist.CtMethod
-import javassist.Modifier
-import javassist.bytecode.Descriptor
-import javassist.bytecode.LocalVariableAttribute
 import org.gradle.api.Project
 import org.slf4j.LoggerFactory
 import java.io.File
@@ -18,10 +13,6 @@ import java.util.jar.JarFile
 class RetropilerTransform(val project: Project) : Transform() {
 
     val logger = LoggerFactory.getLogger(RetropilerPlugin::class.java)!!
-
-    val lambdaClassPattern = Regex(".+\\\$\\\$Lambda\\\$\\d+")
-
-    val lambdaFactoryName = "lambdaFactory$"
 
     override fun getName(): String {
         return "retropiler"
@@ -65,52 +56,11 @@ class RetropilerTransform(val project: Project) : Transform() {
         classPool.appendClassPath(project.rootProject.file("runtime/build/classes/main").absolutePath) // FIXME
 
         val runtime = Runtime(classPool)
-
         val ctClasses = collectClassNames(invocation).map { className -> classPool.get(className) }
-
-        val optionalType = classPool.get("java.util.Optional")
-        val optionalDesc = Descriptor.of(optionalType)
-        val retroOptionalType = classPool.get("io.github.retropiler.runtime.java.util._Optional")
-        val retroOptionalDesc = Descriptor.of(retroOptionalType)
 
         // pre process
         ctClasses.forEach { ctClass ->
-            if (lambdaClassPattern.matches(ctClass.simpleName)) {
-                fixupLambdaClass(runtime, ctClass)
-            }
-
-            // replace java.util.Optional to retropiler's in local variables
-            ctClass.declaredMethods.forEach { method ->
-                val ca = method.methodInfo2.codeAttribute
-                val lva = ca?.getAttribute(LocalVariableAttribute.tag) as LocalVariableAttribute?
-                if (lva != null) {
-
-                    val cp = method.methodInfo2.constPool
-                    val newLva = LocalVariableAttribute(cp)
-                    var entryIndex = 0
-
-                    (0 .. lva.tableLength()  - 1).forEach { i ->
-                        if (lva.descriptor(i) == optionalDesc) {
-                            newLva.addEntry(lva.startPc(i),
-                                    lva.codeLength(i),
-                                    lva.nameIndex(i),
-                                    cp.addUtf8Info(retroOptionalDesc),
-                                    entryIndex)
-                            entryIndex += Descriptor.dataSize(retroOptionalDesc)
-                        } else {
-                            newLva.addEntry(lva.startPc(i),
-                                    lva.codeLength(i),
-                                    lva.nameIndex(i),
-                                    lva.descriptorIndex(i),
-                                    entryIndex)
-                            entryIndex += Descriptor.dataSize(lva.descriptor(i))
-                        }
-                    }
-
-                    lva.set(newLva.get())
-                    ca.maxLocals = entryIndex
-                }
-            }
+            runtime.fixup(ctClass)
         }
 
         // main process
@@ -120,9 +70,7 @@ class RetropilerTransform(val project: Project) : Transform() {
 
         // post process and write it down
         ctClasses.forEach { ctClass ->
-            if (lambdaClassPattern.matches(ctClass.simpleName)) {
-                cleanupLambdaClass(runtime, ctClass)
-            }
+            runtime.cleanup(ctClass)
 
             ctClass.writeFile(outputDir.canonicalPath)
         }
@@ -130,42 +78,6 @@ class RetropilerTransform(val project: Project) : Transform() {
         copyResourceFiles(invocation.inputs, outputDir)
 
         logger.trace("transform: ${System.currentTimeMillis() - t0}ms")
-    }
-
-    // retrolambda generates `Consumer lambdaFactory$(...)` so it replaces the return type
-    // to retropiler runtime classes
-    private fun fixupLambdaClass(runtime: Runtime, lambdaClass: CtClass) {
-        val retroClass = runtime.getRetroClass(lambdaClass.interfaces[0])
-
-        lambdaClass.addInterface(retroClass)
-
-        val lambdaFactory = lambdaClass.getDeclaredMethod(lambdaFactoryName)
-        if (lambdaFactory.parameterTypes.isEmpty()) {
-            val newLambdaFactory = CtMethod.make("""
-                            public static ${lambdaClass.name} _${lambdaFactoryName}() {
-                                return instance;
-                            }
-                        """, lambdaClass)
-            lambdaClass.addMethod(newLambdaFactory)
-        } else {
-            val newLambdaFactory = CtMethod(lambdaClass, "_" + lambdaFactoryName, lambdaFactory.parameterTypes, lambdaClass)
-            newLambdaFactory.modifiers = newLambdaFactory.modifiers.or(Modifier.STATIC)
-            newLambdaFactory.setBody("""
-                            { return new ${lambdaClass.name}($$); }
-                        """)
-            lambdaClass.addMethod(newLambdaFactory)
-        }
-    }
-
-    private fun cleanupLambdaClass(runtime: Runtime, lambdaClass: CtClass) {
-        // assume that lambdaClass implements just the single functional interface
-        val retroClass = runtime.getRetroClass(lambdaClass.interfaces[0])
-
-        lambdaClass.interfaces = lambdaClass.interfaces.filter {
-            it == retroClass
-        }.toTypedArray()
-
-        lambdaClass.removeMethod(lambdaClass.getDeclaredMethod(lambdaFactoryName))
     }
 
     fun collectClassPath(invocation: TransformInvocation): Set<File> {
@@ -237,7 +149,6 @@ class RetropilerTransform(val project: Project) : Transform() {
 
         return list
     }
-
 
     fun copyResourceFiles(inputs: Collection<TransformInput>, outputDir: File) {
         inputs.forEach {
--- plugin/src/main/kotlin/io/github/retropiler/Runtime.kt
+++ plugin/src/main/kotlin/io/github/retropiler/Runtime.kt
@@ -2,9 +2,19 @@ package io.github.retropiler
 
 import javassist.ClassPool
 import javassist.CtClass
+import javassist.CtMethod
+import javassist.Modifier
+import javassist.bytecode.Descriptor
+import javassist.bytecode.LocalVariableAttribute
 
 class Runtime(val classPool: ClassPool) {
     val PACKAGE = "io.github.retropiler.runtime"
+    val lambdaClassPattern = Regex(".+\\\$\\\$Lambda\\\$\\d+")
+    val optionalType = classPool.get("java.util.Optional")!!
+    val optionalDesc = Descriptor.of(optionalType)!!
+    val retroOptionalType = classPool.get("io.github.retropiler.runtime.java.util._Optional")!!
+    val retroOptionalDesc = Descriptor.of(retroOptionalType)!!
+    val lambdaFactoryName = "lambdaFactory$"
 
     fun getRetroClassOrNull(ctClass: CtClass): CtClass? {
         val packageName = ctClass.packageName
@@ -16,5 +26,87 @@ class Runtime(val classPool: ClassPool) {
         val packageName = ctClass.packageName
         val simpleName = ctClass.simpleName
         return classPool.getOrNull("$PACKAGE.$packageName._$simpleName")
+    }
+
+    fun fixup(ctClass: CtClass) {
+        if (lambdaClassPattern.matches(ctClass.simpleName)) {
+            fixupLambdaClass(ctClass)
+        }
+
+        fixupLocalVariableTypes(ctClass)
+    }
+
+    // retrolambda generates `Consumer lambdaFactory$(...)` so it replaces the return type
+    // to retropiler runtime classes
+    private fun fixupLambdaClass(lambdaClass: CtClass) {
+        val retroClass = getRetroClass(lambdaClass.interfaces[0])
+
+        lambdaClass.addInterface(retroClass)
+
+        val lambdaFactory = lambdaClass.getDeclaredMethod(lambdaFactoryName)
+        if (lambdaFactory.parameterTypes.isEmpty()) {
+            val newLambdaFactory = CtMethod.make("""
+                            public static ${lambdaClass.name} _${lambdaFactoryName}() {
+                                return instance;
+                            }
+                        """, lambdaClass)
+            lambdaClass.addMethod(newLambdaFactory)
+        } else {
+            val newLambdaFactory = CtMethod(lambdaClass, "_" + lambdaFactoryName, lambdaFactory.parameterTypes, lambdaClass)
+            newLambdaFactory.modifiers = newLambdaFactory.modifiers.or(Modifier.STATIC)
+            newLambdaFactory.setBody("""
+                            { return new ${lambdaClass.name}($$); }
+                        """)
+            lambdaClass.addMethod(newLambdaFactory)
+        }
+    }
+
+    private fun fixupLocalVariableTypes(ctClass: CtClass) {
+        ctClass.declaredMethods.forEach { method ->
+            val ca = method.methodInfo2.codeAttribute
+            val lva = ca?.getAttribute(LocalVariableAttribute.tag) as LocalVariableAttribute?
+            if (lva != null) {
+
+                val cp = method.methodInfo2.constPool
+                val newLva = LocalVariableAttribute(cp)
+                var entryIndex = 0
+
+                (0..lva.tableLength() - 1).forEach { i ->
+                    if (lva.descriptor(i) == optionalDesc) {
+                        newLva.addEntry(lva.startPc(i),
+                                lva.codeLength(i),
+                                lva.nameIndex(i),
+                                cp.addUtf8Info(retroOptionalDesc),
+                                entryIndex)
+                        entryIndex += Descriptor.dataSize(retroOptionalDesc)
+                    } else {
+                        newLva.addEntry(lva.startPc(i),
+                                lva.codeLength(i),
+                                lva.nameIndex(i),
+                                lva.descriptorIndex(i),
+                                entryIndex)
+                        entryIndex += Descriptor.dataSize(lva.descriptor(i))
+                    }
+                }
+
+                lva.set(newLva.get())
+                ca.maxLocals = entryIndex
+            }
+        }
+    }
+
+     fun cleanup(ctClass: CtClass) {
+        if (!lambdaClassPattern.matches(ctClass.simpleName)) {
+            return
+        }
+
+        // assume that lambdaClass implements just the single functional interface
+        val retroClass = getRetroClass(ctClass.interfaces[0])
+
+        ctClass.interfaces = ctClass.interfaces.filter {
+            it == retroClass
+        }.toTypedArray()
+
+        ctClass.removeMethod(ctClass.getDeclaredMethod(lambdaFactoryName))
     }
 }
